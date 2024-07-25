979298c79e1f817167baf8a552ea11e7069a2e2a
nyberry <nckbrry@gmail.com>
2024-07-25 19:13:07 +0300

    fix error that hint buttons appear although solved

--- numbler/urls.py
+++ numbler/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from numbler import views
 
 urlpatterns = [
-    path('', views.numbler, name='numbler'),
+    path('', views.welcome, name='numbler'),
     path('numbler/', views.numbler, name = "numbler"),
     path('next_puzzle/', views.next_puzzle, name = "next_puzzle"),    
 ]
--- numbler/views.py
+++ numbler/views.py
@@ -1,30 +1,41 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 import json
 import datetime
 
-def numbler(request):
-    puzzles = {'2024-07-25': {'Tiles': ['2', '2', '6', '5'], 'Easy': (14, ['2+2*6', '2+6*2', '2*6+2', '6*2+2']), 'Medium': (20, ['52/2-6']), 'Hard': (688, ['6!-2^5'])}, '2024-07-26': {'Tiles': ['3', '3', '2', '4'], 'Easy': (10, ['3*2+4', '3*4-2', '2*3+4', '4+3*2', '4+2*3', '4*3-2']), 'Medium': (95, ['3+23*4', '3+4*23', '23*4+3', '4*23+3']), 'Hard': (228, ['234-3!'])}, '2024-07-27': {'Tiles': ['4', '1', '3', '5'], 'Easy': (16, ['1+3*5', '1+5*3', '3*5+1', '5*3+1']), 'Medium': (37, ['14*3-5', '3*14-5']), 'Hard': (219, ['3^5-4!'])}, '2024-07-28': {'Tiles': ['1', '9', '8', '4'], 'Easy': (16, ['9+8-1', '9-1+8', '8+9-1', '8-1+9']), 'Medium': (56, ['9+48-1', '9-1+48', '8+49-1', '8-1+49', '49+8-1', '49-1+8', '48+9-1', '48-1+9']), 'Hard': (183, ['8*4!-9', '4!*8-9'])}, '2024-07-29': {'Tiles': ['1', '4', '6', '1'], 'Easy': (9, ['4+6-1', '4-1+6', '6+4-1', '6-1+4']), 'Medium': (83, ['14*6-1', '6*14-1']), 'Hard': (92, ['116-4!'])}}
+def welcome(request):
+
+    puzzles = {'2024-07-25': {'Tiles': ['2', '2', '6', '5'], 'Easy': (14, ['6*2+2', '2+6*2', '2*6+2', '2+2*6']), 'Medium': (20, ['52/2-6']), 'Hard': (688, ['6!-2^5'])}, '2024-07-26': {'Tiles': ['3', '3', '2', '4'], 'Easy': (10, ['3*2+4', '3*4-2', '2*3+4', '4+3*2', '4+2*3', '4*3-2']), 'Medium': (95, ['3+23*4', '3+4*23', '23*4+3', '4*23+3']), 'Hard': (228, ['234-3!'])}, '2024-07-27': {'Tiles': ['4', '1', '3', '5'], 'Easy': (16, ['1+3*5', '1+5*3', '3*5+1', '5*3+1']), 'Medium': (37, ['14*3-5', '3*14-5']), 'Hard': (219, ['3^5-4!'])}, '2024-07-28': {'Tiles': ['1', '9', '8', '4'], 'Easy': (16, ['9+8-1', '9-1+8', '8+9-1', '8-1+9']), 'Medium': (56, ['9+48-1', '9-1+48', '8+49-1', '8-1+49', '49+8-1', '49-1+8', '48+9-1', '48-1+9']), 'Hard': (183, ['8*4!-9', '4!*8-9'])}, '2024-07-29': {'Tiles': ['1', '4', '6', '1'], 'Easy': (9, ['4+6-1', '4-1+6', '6+4-1', '6-1+4']), 'Medium': (83, ['14*6-1', '6*14-1']), 'Hard': (92, ['116-4!'])}}
+    
     today = datetime.date.today()
     date_str = today.strftime("%Y-%m-%d")
+    
     puzzle = puzzles.get(date_str)
-    if puzzle == None:
-        puzzle = puzzles['2024-07-24']
+    print(puzzle)
 
+    if puzzle == None:
+        puzzle = puzzles['2024-07-25']
+
+    request.session['difficulty']='Easy'
+    request.session['puzzle']=puzzle
+    request.session['date_str']=date_str
+
+    return redirect('numbler')
+
+
+def numbler(request):
+    
+    puzzle = request.session['puzzle']
     num_tiles = puzzle['Tiles']
 
-    # Initialize session variable for difficulty if not set, or if completed
     if 'difficulty' not in request.session:
         request.session['difficulty'] = 'Easy'
-
-    if request.session['difficulty'] == "Completed":
-        request.session['difficulty'] = 'Easy'
-
-
-
     
     difficulty = request.session['difficulty']
+
     result = puzzle[difficulty][0]
     expressions = puzzle[difficulty][1]
+
     boxes = [tile for tile in expressions[0]]
 
     context = {
@@ -37,10 +48,8 @@ def numbler(request):
 
     return render(request, 'numbler.html', context)
 
-def next_puzzle(request):
 
-    today = datetime.date.today()
-    date_str = today.strftime("%Y-%m-%d")
+def next_puzzle(request):
 
     if 'hints' not in request.session:
         request.session['hints'] = ['-1','-1','-1']
@@ -103,6 +112,7 @@ def next_puzzle(request):
                 result = "❌"
             hints_message.append(result)    
         
+        date_str = request.session['date_str']
         result_message = f'NumGoose 🪿 {date_str}\n\nEasy: {hints_message[0]}\nMedium: {hints_message[1]}\nHard: {hints_message[2]}\n\nplay NumGoose 🪿\nnyberry.pythonanywhere.com'
             
         next_game_message = f"Next puzzle in {hours} hours and {minutes} minutes"
