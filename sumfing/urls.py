9e69288a7df26fed1bcf735006ab9ed056ca2d24
nyberry <nckbrry@gmail.com>
2024-07-29 00:28:57 +0300

    make routes secure & add timer

--- sumfing/urls.py
+++ sumfing/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from sumfing import views
 
 urlpatterns = [
-    path('', views.welcome, name='sumfing'),
+    path('', views.index, name='index'),
     path('sumfing/', views.sumfing, name = "sumfing"),
-    path('next_puzzle/', views.next_puzzle, name = "next_puzzle"),    
+    path('next_puzzle/', views.next_puzzle, name = "next_puzzle"),
+    path('completed/', views.completed, name = "completed"),    
 ]
--- sumfing/views.py
+++ sumfing/views.py
@@ -2,47 +2,64 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 import json
 import datetime
+import time
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
-def welcome(request):
+def index(request):
 
-    puzzles = {'2024-07-25': {'Tiles': ['2', '2', '6', '5'], 'Easy': (14, ['6*2+2', '2+6*2', '2*6+2', '2+2*6']), 'Medium': (20, ['52/2-6']), 'Hard': (688, ['6!-2^5'])}, '2024-07-26': {'Tiles': ['3', '3', '2', '4'], 'Easy': (10, ['3*2+4', '3*4-2', '2*3+4', '4+3*2', '4+2*3', '4*3-2']), 'Medium': (95, ['23*4+3', '4*23+3','3+23*4', '3+4*23']), 'Hard': (228, ['234-3!'])}, '2024-07-27': {'Tiles': ['4', '1', '3', '5'], 'Easy': (16, ['1+3*5', '1+5*3', '3*5+1', '5*3+1','4*3+1','4*1+3','3+1*4','1+3*4','5-1*4','4*5-1']), 'Medium': (37, ['14*3-5', '3*14-5']), 'Hard': (219, ['3^5-4!'])}, '2024-07-28': {'Tiles': ['1', '9', '8', '4'], 'Easy': (16, ['9+8-1', '9-1+8', '8+9-1', '8-1+9']), 'Medium': (56, ['9+48-1', '9-1+48', '8+49-1', '8-1+49', '49+8-1', '49-1+8', '48+9-1', '48-1+9']), 'Hard': (183, ['8*4!-9', '4!*8-9'])}, '2024-07-29': {'Tiles': ['1', '4', '6', '1'], 'Easy': (9, ['4+6-1', '4-1+6', '6+4-1', '6-1+4']), 'Medium': (83, ['14*6-1', '6*14-1']), 'Hard': (92, ['116-4!'])}}
-    
     today = datetime.date.today()
-    date_str = today.strftime("%Y-%m-%d")
-    
-    puzzle = puzzles.get(date_str)
+    today_str = today.strftime("%Y-%m-%d")
+
+    puzzles = {'2024-07-25': {'Tiles': ['2', '2', '6', '5'], 'Easy': (14, ['6*2+2', '2+6*2', '2*6+2', '2+2*6']), 'Medium': (20, ['52/2-6']), 'Hard': (688, ['6!-2^5'])}, '2024-07-26': {'Tiles': ['3', '3', '2', '4'], 'Easy': (10, ['3*2+4', '3*4-2', '2*3+4', '4+3*2', '4+2*3', '4*3-2']), 'Medium': (95, ['23*4+3', '4*23+3','3+23*4', '3+4*23']), 'Hard': (228, ['234-3!'])}, '2024-07-27': {'Tiles': ['4', '1', '3', '5'], 'Easy': (16, ['1+3*5', '1+5*3', '3*5+1', '5*3+1','4*3+1','4*1+3','3+1*4','1+3*4','5-1*4','4*5-1']), 'Medium': (37, ['14*3-5', '3*14-5']), 'Hard': (219, ['3^5-4!'])}, '2024-07-28': {'Tiles': ['1', '9', '8', '4'], 'Easy': (16, ['9+8-1', '9-1+8', '8+9-1', '8-1+9']), 'Medium': (56, ['9+48-1', '9-1+48', '8+49-1', '8-1+49', '49+8-1', '49-1+8', '48+9-1', '48-1+9']), 'Hard': (183, ['8*4!-9', '4!*8-9'])}, '2024-07-29': {'Tiles': ['1', '4', '6', '1'], 'Easy': (9, ['4+6-1', '4-1+6', '6+4-1', '6-1+4']), 'Medium': (83, ['14*6-1', '6*14-1']), 'Hard': (92, ['116-4!'])}}    
+    puzzle = puzzles.get(today_str)
 
     if puzzle == None:
         puzzle = puzzles['2024-07-25']
 
-    start_date = datetime.datetime.strptime("2024-07-25", "%Y-%m-%d").date()
-    days_since_start = (today - start_date).days
+    date_of_first_puzzle = datetime.datetime.strptime("2024-07-25", "%Y-%m-%d").date()
+    days_since_start = (today - date_of_first_puzzle).days
 
+    # check if user has ever visited before:
+    if 'date_str' in request.session:
+
+        # if user has already visited today, redirect to game play page:
+        if request.session['date_str']==today_str:
+            return redirect('sumfing') 
+            
+    # this is user's first visit today, so reset the session data
+    current_time=time.time()
     request.session['difficulty']='Easy'
     request.session['puzzle']=puzzle
-    request.session['date_str']=date_str
+    request.session['date_str']=today_str
     request.session['game_no']=days_since_start
-
+    request.session['start_time']=current_time
     return redirect('sumfing')
+
 
 @csrf_protect
 def sumfing(request):
-    
-    puzzle = request.session['puzzle']
-    num_tiles = puzzle['Tiles']
 
-    if 'difficulty' not in request.session:
-        request.session['difficulty'] = 'Easy'
+    # redirect if the user has come here by entering route in browser, without starting a session
+    if 'date_str' not in request.session:
+        return redirect ('index')
     
-    difficulty = request.session['difficulty']
+    # get the session data, redirect if a problem
+    try:
+        puzzle = request.session['puzzle']
+        num_tiles = puzzle['Tiles']
+        difficulty = request.session['difficulty']
+    except:
+        return redirect ('index')
 
+    # redirect if the game is finished
+    if difficulty == "Completed":
+        return redirect('completed')
+
+    # render the puzzle page
     result = puzzle[difficulty][0]
     expressions = puzzle[difficulty][1]
-
     boxes = [tile for tile in expressions[0]]
-
     context = {
         'difficulty': difficulty.lower(),
         'num_tiles': num_tiles,
@@ -50,12 +67,17 @@ def sumfing(request):
         'result': result,
         'expressions': json.dumps(expressions)
     }
-
     return render(request, 'sumfing.html', context)
+
 
 @csrf_protect
 def next_puzzle(request):
 
+    # redirect if not here by form button
+    if request.method != 'POST':
+        return redirect('index')
+    
+    # OK to move to next puzzle
     if 'hints' not in request.session:
         request.session['hints'] = ['-1','-1','-1']
 
@@ -64,17 +86,14 @@ def next_puzzle(request):
     
     current_difficulty = request.session['difficulty']
 
-    # update the results
-    if request.method == 'POST':
-        hint_level = request.POST.get('hint_level')
-        
-        if current_difficulty == 'Easy':
-            request.session['hints'][0]= hint_level
-        elif current_difficulty == 'Medium':
-            request.session['hints'][1]= hint_level
-        elif current_difficulty == 'Hard':
-            request.session['hints'][2]= hint_level
-
+    # keep track of how many hints were given
+    hint_level = request.POST.get('hint_level')    
+    if current_difficulty == 'Easy':
+        request.session['hints'][0]= hint_level
+    elif current_difficulty == 'Medium':
+        request.session['hints'][1]= hint_level
+    elif current_difficulty == 'Hard':
+        request.session['hints'][2]= hint_level
 
     # Update the difficulty level for the next puzzle
     if current_difficulty == 'Easy':
@@ -83,54 +102,66 @@ def next_puzzle(request):
         request.session['difficulty'] = 'Hard'
     elif current_difficulty == 'Hard':
         request.session['difficulty'] = 'Completed'
-
+        current_time = time.time()
+        time_taken = int(current_time- request.session['start_time'])
+        request.session['time_taken'] = time_taken
 
     # If the puzzle completed
     if request.session['difficulty'] == 'Completed':
-        
-        # Get the current date and time
-        now = datetime.datetime.now()
-
-        # Get the start of tomorrow
-        tomorrow = (now + datetime.timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
-
-        # Calculate the time difference
-        time_difference = tomorrow - now
-
-        # Convert the difference to hours and minutes
-        total_seconds = time_difference.total_seconds()
-        hours = int(total_seconds // 3600)
-        minutes = int((total_seconds % 3600) // 60)
-        hints_given = request.session['hints']
-        print (f'Hints given: {hints_given}')
-        hints_message = []
-        for i in range(len(hints_given)):
-            if hints_given[i]=='-1':
-                result = "?"
-            elif hints_given[i]=='0':
-                result = "✅"
-            elif hints_given[i]=='1':
-                result =f'💡'
-            elif hints_given[i]=='2':
-                result =f'💡💡'
-            elif hints_given[i]=='3':
-                result = "❌"
-            hints_message.append(result)    
-        
-        date_str = request.session['date_str']
-        game_no = request.session['game_no']
-        html_message = f'Sumfing #{game_no} {date_str}\n\nEasy: {hints_message[0]}\nMedium: {hints_message[1]}\nHard: {hints_message[2]}\n\n'
-        whatsapp_message = f'Sumfing #{game_no} {date_str}\nEasy: {hints_message[0]}\nMedium: {hints_message[1]}\nHard: {hints_message[2]}\nPlay at sumfing.com'
-           
-        next_game_message = f"Sumfing else in\n{hours} hours and {minutes} minutes"
-
-
-        context = {
-            'html_message':html_message,
-            'whatsapp_message':whatsapp_message,
-            'next_game_message':next_game_message,
-        }
-
-        return render (request, 'completed.html', context)
-        
+        return redirect('completed')
+                
     return sumfing(request)
+
+
+def completed(request):
+
+    # redirect if the user came here by browswer line
+    if 'difficulty' not in request.session:
+        return redirect('index')
+    
+    if request.session['difficulty'] != "Completed":
+        return redirect('index')
+
+    # Make a message about how many hints have been given
+    hints_given = request.session['hints']
+    hints_message = []
+    for i in range(len(hints_given)):
+        if hints_given[i]=='-1':
+            result = "?"
+        elif hints_given[i]=='0':
+            result = "✅"
+        elif hints_given[i]=='1':
+            result =f'💡'
+        elif hints_given[i]=='2':
+            result =f'💡💡'
+        elif hints_given[i]=='3':
+            result = "❌"
+        hints_message.append(result)    
+    
+    game_no = request.session['game_no']
+    time_taken = request.session['time_taken']
+    html_message = f'Sumfing #{game_no}\n\nEasy: {hints_message[0]}\nMedium: {hints_message[1]}\nHard: {hints_message[2]}\n\nTime: {time_taken} seconds'
+    whatsapp_message = f'Sumfing #{game_no}\nEasy: {hints_message[0]}\nMedium: {hints_message[1]}\nHard: {hints_message[2]}\nTime: {time_taken} seconds\nPlay at sumfing.com'
+
+    # Make next game message:
+    # 1. Get the current date and time
+    now = datetime.datetime.now()
+    # 2. Get the start of tomorrow
+    tomorrow = (now + datetime.timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+    # 3. Calculate the time difference
+    time_difference = tomorrow - now
+    # 4. Convert the difference to hours and minutes
+    total_seconds = time_difference.total_seconds()
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) // 60)
+    # 5. Form the message string
+    next_game_message = f'Sumfing else in\n{hours} hours and {minutes} minutes'
+
+    context = {
+        'html_message':html_message,
+        'whatsapp_message':whatsapp_message,
+        'next_game_message':next_game_message,
+    }
+
+    return render (request, 'completed.html', context)
+
